12e784cc4f435d1910ac8ae2b6e3229ce1b5235a
Genadi Samokovarov <gsamokovarov@gmail.com>
2014-03-26 11:32:52 +0200

    Puppet: Show off ordering of resource references

--- provisioning/puppet/manifests/default.pp
+++ provisioning/puppet/manifests/default.pp
@@ -1,9 +1,14 @@
 node default {
-  exec { '/usr/bin/apt-get update': } -> class { 'apt': always_apt_update => true }
+  exec { '/usr/bin/apt-get update': }
 
+  include apt
   class { 'uwsgi':
     wsgi_file => '/vagrant/src/app.py'
   }
-
   include nginx
+
+  # You can use the arrow operator on resource references too. Those aren't
+  # parser dependant.
+  Exec['/usr/bin/apt-get update'] -> Class['apt']
+
 }
